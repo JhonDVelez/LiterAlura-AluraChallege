af67f4493e2b4e1aa42a94bbac4fc50dcc97de3c
Jhon D. Velez <jvelez135@gmail.com>
2024-07-04 10:55:06 -0500

    Creación del proyecto

---
+++ src/test/java/com/jdvelez/LiterAlura_AluraChallenge/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdvelez.LiterAlura_AluraChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
